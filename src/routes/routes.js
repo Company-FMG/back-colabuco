823d8bf32e9afd33090447ccfce27dc796f776c0
Nicholas <nicholasbergqvist14@gmail.com>
2024-05-16 12:08:13 -0300

    Model produto e router produto feitos

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,16 +1,16 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
 
-import pedidosRouter from './pedidos.js'
-//import produtosRouter from './produtos.js (Exemplo para o resto da equipe)'
+import pedidosRouter from "./pedidos.js";
+import produtosRouter from "./produto.js";
 
 router.use(express.json());
 
-router.get('/', (req, res) => {
-    res.send("Back-end do colabuco!")
-})
+router.get("/", (req, res) => {
+  res.send("Back-end do colabuco!");
+});
 
-router.use('/pedidos', pedidosRouter);
-//router.use('/produtos', produtosRouter (Exemplo para o resto da equipe);
+router.use("/pedidos", pedidosRouter);
+router.use("/produtos", produtosRouter);
 
 export default router;
